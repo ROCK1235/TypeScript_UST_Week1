c7d230a9a3bfb401c100b557fe84a46da9ed6567
Ayush Kumar Dash(UST <308233@ust.com>
2026-03-24 17:52:10 +0530

    added some functionalities

--- PLAYWRIGHT/tests/pages.spec.ts
+++ PLAYWRIGHT/tests/pages.spec.ts
@@ -1,27 +1,27 @@
 import {test,expect, Page, BrowserContext} from "@playwright/test"
  
-// test("Handling Windows" , async ({browser}) => {
+test("Handling Windows" , async ({browser}) => {
  
-//     const context: BrowserContext = await browser.newContext();
-//     const page: Page = await context.newPage();
+    const context: BrowserContext = await browser.newContext();
+    const page: Page = await context.newPage();
  
-//     await page.goto("https://demo.automationtesting.in/Windows.html");
-//     const parentPage = page.url();
-//     console.log(parentPage);
-//     const [newPage] = await Promise.all([
-//         context.waitForEvent('page'),
-//         page.click('button:has-text("click")')
-//     ]);
+    await page.goto("https://demo.automationtesting.in/Windows.html");
+    const parentPage = page.url();
+    console.log(parentPage);
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        page.click('button:has-text("click")')
+    ]);
 
-//     await newPage.waitForLoadState();
-//     console.log(newPage.url());
-//     await newPage.locator("//a[@class='selenium-button selenium-ide text-uppercase fw-bold']");
-//     await newPage.waitForTimeout(3000);
+    await newPage.waitForLoadState();
+    console.log(newPage.url());
+    await newPage.locator("//a[@class='selenium-button selenium-ide text-uppercase fw-bold']");
+    await newPage.waitForTimeout(3000);
  
-//     await page.bringToFront();
-//     await page.locator("//a[@href='#Seperate']").click();
-//     await page.waitForTimeout(3000);
-// });
+    await page.bringToFront();
+    await page.locator("//a[@href='#Seperate']").click();
+    await page.waitForTimeout(3000);
+});
 test("Handling Multiple Windows" , async ({browser}) => {
  
     const context: BrowserContext = await browser.newContext();
--- PLAYWRIGHT/tests/practo.spec.ts
+++ PLAYWRIGHT/tests/practo.spec.ts
@@ -86,21 +86,58 @@ import {test, expect} from "@playwright/test";
 //     // console.log("Total Genders: " + await totalGenders.count());
 // });
 
-test("Test Book Doctor Page",async ({ page }) => {
+// test("Test Book Doctor Page",async ({ page }) => {
+//     await page.goto("https://practo.com");
+
+//     //await page.waitForTimeout(5000)
+    
+//     //await page.locator(".product-tab__title").click();
+//     await page.locator("//div[@class='product-tab']/child::a[@title='book']").click();
+   
+//     await page.getByPlaceholder("Search location").fill("Hyderabad");
+//     await page.locator("//div[@data-qa-id='omni-suggestion-main'and @class='c-omni-suggestion-item__content__title'and text()='Hyderabad']").click();
+//     await page.getByPlaceholder("Search doctors, clinics, hospitals, etc.").fill("Dentist");
+//     await page.locator("//div[@data-qa-id='omni-suggestion-main'and @class='c-omni-suggestion-item__content__title'and text()='Dentist']").click();
+//     await page.locator("//div[contains(@class,'u-color--primary uv2-spacer--xs-bottom')]/child::h2[text()='Dr. Chandrashekhar Reddy']").click();
+//     const slots=await page.locator("//div[text()='today']/following-sibling::div[contains(@class,'slot-count')]//span");
+//     await expect(slots).toBeVisible();
+//     await page.waitForTimeout(10000);
+
+// });
+
+
+test("Test Upload Files ",async ({ page }) => {
     await page.goto("https://practo.com");
 
     //await page.waitForTimeout(5000)
     
     //await page.locator(".product-tab__title").click();
-    await page.locator("//div[@class='product-tab']/child::a[@title='book']").click();
-   
-    await page.getByPlaceholder("Search location").fill("Hyderabad");
-    await page.locator("//div[@data-qa-id='omni-suggestion-main'and @class='c-omni-suggestion-item__content__title'and text()='Hyderabad']").click();
-    await page.getByPlaceholder("Search doctors, clinics, hospitals, etc.").fill("Dentist");
-    await page.locator("//div[@data-qa-id='omni-suggestion-main'and @class='c-omni-suggestion-item__content__title'and text()='Dentist']").click();
-    await page.locator("//div[contains(@class,'u-color--primary uv2-spacer--xs-bottom')]/child::h2[text()='Dr. Chandrashekhar Reddy']").click();
-    const slots=await page.locator("//div[text()='today']/following-sibling::div[contains(@class,'slot-count')]//span");
-    await expect(slots).toBeVisible();
+    await page.locator("//a[text()='Login / Signup']").click();
+    await page.getByPlaceholder("Mobile Number / Email ID").fill("9438625192");
+    await page.getByPlaceholder("Password").fill("Ayush1235@");
+    await page.locator("//label[@class='mousePointer textMuted line-height-20']").click();
+    await page.locator("//button[@type='submit']").click();
+
+    await page.locator("//div[@class='nav-items u-d-trigger dropdown-toggle push-droparrow']").click();
+    await page.getByRole('link', { name: 'My Medical Records' }).click();
+
+//Locating upload files button and upload files
+    await page.locator("//div[@class='button text-center heading-four btn-positive']").click();
+
+    await page.locator("//div[@class='record-upload-add-tile text-small text-center']").click();
+    const fileUpload=await page.locator("//div[@class='record-upload-add-tile text-small text-center']");
+     await fileUpload.setInputFiles("C:\\Users\\308233\\Downloads\\Book1.pdf");
+    //await page.locator("input[type='file']").setInputFiles("C:\\Users\\308233\\Downloads\\Book1.pdf");
+    await page.waitForTimeout(3000);
+
+
+    
+
+    // await page.getByPlaceholder("Search doctors, clinics, hospitals, etc.").fill("Dentist");
+    // await page.locator("//div[@data-qa-id='omni-suggestion-main'and @class='c-omni-suggestion-item__content__title'and text()='Dentist']").click();
+    // await page.locator("//div[contains(@class,'u-color--primary uv2-spacer--xs-bottom')]/child::h2[text()='Dr. Chandrashekhar Reddy']").click();
+    // const slots=await page.locator("//div[text()='today']/following-sibling::div[contains(@class,'slot-count')]//span");
+    // await expect(slots).toBeVisible();
     await page.waitForTimeout(10000);
 
 });
